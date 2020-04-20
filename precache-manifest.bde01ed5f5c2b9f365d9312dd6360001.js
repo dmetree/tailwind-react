896d5f3b085ec44a343ffdeaefdc3e8556e4893c
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-04-20 12:40:03 +0200

    Updates

---
+++ precache-manifest.bde01ed5f5c2b9f365d9312dd6360001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58952e436fcfdd7e09d78fc5d6fba7cd",
+    "url": "/tailwind-react/index.html"
+  },
+  {
+    "revision": "a1a24fd39e147a447a98",
+    "url": "/tailwind-react/static/css/main.096afcfd.chunk.css"
+  },
+  {
+    "revision": "794475f9a79341cd3092",
+    "url": "/tailwind-react/static/js/2.dd1275c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tailwind-react/static/js/2.dd1275c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a24fd39e147a447a98",
+    "url": "/tailwind-react/static/js/main.1aa6c071.chunk.js"
+  },
+  {
+    "revision": "634d42ebd12922b85921",
+    "url": "/tailwind-react/static/js/runtime-main.8c9cf70d.js"
+  }
+]);
